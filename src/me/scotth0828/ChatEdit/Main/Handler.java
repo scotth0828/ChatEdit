cc57feb21b652752fdd1658a06fca3b2a25230fd
scotth0828 <scotth0828@gmail.com>
2017-01-07 07:04:54 -0500

    bug fixes and welcome message

--- src/me/scotth0828/ChatEdit/Main/Handler.java
+++ src/me/scotth0828/ChatEdit/Main/Handler.java
@@ -6,6 +6,8 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 
+import net.md_5.bungee.api.ChatColor;
+
 public class Handler implements Listener {
 
 	Main main;
@@ -35,12 +37,10 @@ public class Handler implements Listener {
 			if (PLType.equals("global")) {
 				pl.sendMessage(fMessage);
 			} else if (PLType.equals("nearby")) {
-				if (pl.getPlayer().getLocation().distance(e.getPlayer().getLocation()) <= main.getConfig()
+				if (pl.getWorld() == e.getPlayer().getWorld() && pl.getPlayer().getLocation().distance(e.getPlayer().getLocation()) <= main.getConfig()
 						.getInt("ChatEdit.Default.Radius")) {
 					pl.sendMessage(fMessage);
 				}
-			} else if (PLType.equals("off")) {
-				return;
 			}
 
 		}
@@ -61,6 +61,9 @@ public class Handler implements Listener {
 					main.getConfig().getString("ChatEdit.Default.Radius"));
 			main.users.saveData();
 		}
+
+		e.getPlayer().sendMessage(ChatColor.YELLOW + "[ChatEdit]" + ChatColor.GREEN + " Your chat type is set to "
+				+ ChatColor.BLUE + main.users.getData().getString(e.getPlayer().getUniqueId() + ".type"));
 
 	}
 
